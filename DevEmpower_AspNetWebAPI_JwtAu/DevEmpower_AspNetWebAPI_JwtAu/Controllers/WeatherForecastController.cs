84068b740add48dd3d51120c4c604c92a10b40b1
AndriySham <avshamatrin@gmail.com>
2023-12-20 11:36:57 +0200

    refactor Wether resourses

--- DevEmpower_AspNetWebAPI_JwtAu/DevEmpower_AspNetWebAPI_JwtAu/Controllers/WeatherForecastController.cs
+++ DevEmpower_AspNetWebAPI_JwtAu/DevEmpower_AspNetWebAPI_JwtAu/Controllers/WeatherForecastController.cs
@@ -9,25 +9,24 @@ namespace DevEmpower_AspNetWebAPI_JwtAu.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        #region common part is hidden
+        //private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        #endregion
+
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
         {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
         }
     }
 }
