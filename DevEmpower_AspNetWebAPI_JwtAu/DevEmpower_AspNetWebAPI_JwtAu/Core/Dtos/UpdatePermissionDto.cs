769550fb63db6ddeafde2b23d7d202a8955868c4
AndriySham <avshamatrin@gmail.com>
2023-12-20 18:04:32 +0200

    Add Dtos and static Roles

---
+++ DevEmpower_AspNetWebAPI_JwtAu/DevEmpower_AspNetWebAPI_JwtAu/Core/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevEmpower_AspNetWebAPI_JwtAu.Core.Dtos
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "User is required")]
+        public string UserName { get; set; }
+    }
+}
